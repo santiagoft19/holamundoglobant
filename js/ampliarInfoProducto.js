f723fd6eb8252900b048a6874b304758ae90b053
santiagoft19 <floreztamayosantiago@gmail.com>
2022-03-01 00:36:28 -0500

    Se adelanta tienda virtual y se adelanta el modal

--- js/ampliarInfoProducto.js
+++ js/ampliarInfoProducto.js
@@ -3,7 +3,7 @@ export function ampliarInfoProducto(event){
     //console.log(event.target.classList)
     
     if(event.target.classList.contains("btn")){
-        console.log("c")
+        console.log(event.target.parentElement.querySelector("h1").textContent)
     }
 
 }
